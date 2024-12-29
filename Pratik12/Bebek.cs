1db9172d2d77e4eaf22cfc42c33267f8772c9473
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2024-12-30 02:06:09 +0300

    Add project files.

---
+++ Pratik12/Bebek.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik12
+{
+    public class Bebek
+    {
+        public Bebek()
+        {
+            DogumTarihi = DateOnly.FromDateTime(DateTime.Now);
+            Console.WriteLine(" Ingaaaa ");
+        }
+
+        public Bebek(string ad, string soyad)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            DogumTarihi = DateOnly.FromDateTime(DateTime.Now);
+            Console.WriteLine(" Ingaaaa ");
+        }
+
+        public string? Ad { get; set; }
+        public string? Soyad { get; set; }
+        public DateOnly DogumTarihi { get; set; }
+
+        public void DisplayProperties()
+        {
+            Console.WriteLine($"Ad: {Ad ?? "-"}, Soyad: {Soyad ?? "-"}, Doğum Tarihi: {DogumTarihi}");
+        }
+    }
+}
---
+++ Pratik12/Program.cs
@@ -0,0 +1,8 @@
+﻿using Pratik12; // Importing the namespace Pratik12
+
+Bebek bebek1 = new Bebek(); // Creating an instance of the Bebek class using the default constructor
+bebek1.DisplayProperties(); // Calling the DisplayProperties method to display the properties of bebek1
+
+
+Bebek bebek2 = new Bebek("Ali", "Veli"); // Creating another instance of the Bebek class using the parameterized constructor
+bebek2.DisplayProperties(); // Calling the DisplayProperties method to display the properties of bebek2
